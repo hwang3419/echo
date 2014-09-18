64489e35de1d5e6b83175e16c82f3862f61cef07
hwang3419 <hwang.iit@gmail.com>
2014-09-18 16:19:24 -0500

    add v2

--- test.py
+++ test.py
@@ -1,51 +1,38 @@
 #!/usr/bin/python
- 
-import aiml
+import aiml #as aiml
 import yaml
-_globalSessionID = "_global"
-def getBotPredicate(self,name):
-    """Retrieve the value of the specified bot predicate.
+from aiml import AimlParser
 
-    If name is not a valid bot predicate, the empty string is returned.        
-
-    """
-    try: return self._botPredicates[name]
-    except KeyError,e:
-        print 'error',e
-        return ""
-def getPredicate(self, name, sessionID = _globalSessionID):
-    """Retrieve the current value of the predicate 'name' from the
-    specified session.
-
-    If name is not a valid predicate in the session, the empty
-    string is returned.
-
-    """
-    try: return self._sessions[sessionID][name]
-    except KeyError, e:
-        print 'error',e
-        return ""
-aiml.Kernel.getBotPredicate = getBotPredicate
-aiml.Kernel.getPredicate = getPredicate
 k = aiml.Kernel()
 config = yaml.safe_load(open('config.yaml'))
 # load the aiml file
-from glob import glob
-aimllist = glob('./aiml/*.aiml')
-#for i in aimllist:
-#    k.learn(i)
-k.loadBrain('./brain')
-k.learn("aiml/c.aiml")
+'''from glob import glob
+aimllist = glob('./aimlv2/*.aiml')
+
+
 for key, value in config.items():
-    #print key,value
     k.setPredicate(key, value)
     k.setBotPredicate(key, value)
 
-#k.saveBrain('./brain')
+firstlist = sorted(glob('./aimlv2/reduction*'))
+print firstlist
+secondlist = sorted(glob('./aimlv2/mp*.aiml'))
+print secondlist
+for f in firstlist:
+    k.learn(f)
+    aimllist.remove(f)
+for f in secondlist:
+    k.learn(f)
+    aimllist.remove(f)
+for f in aimllist:
+    k.learn(f)'''
 print dir(k)
-print k.getBotPredicate('master')
-print k.respond('tell me about yourself')
+k.loadBrain('./brainV2')
+
+
 from pprint import pprint
 #pprint(k._botPredicates)
 #while True: print k.respond(raw_input("> "))
 
+
+while True: print k.respond(raw_input(">"))
